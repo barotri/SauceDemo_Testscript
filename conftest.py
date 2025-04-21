13fc0a5c734d942bbf2b5580ccb757b958012659
Baro Tri <baroacc2@gmail.com>
2025-04-21 17:07:04 +0700

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,7 +1,7 @@
 import os
 from datetime import datetime
 
-def pytest_configure(config):
+#def pytest_configure(config):
     # Đặt tên file report với định dạng: report_YYYY-MM-DD_HH-MM-SS.html
-    current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    config.option.htmlpath = f"reports/report_{current_time}.html"
+#    current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+#    config.option.htmlpath = f"reports/report_{current_time}.html"
