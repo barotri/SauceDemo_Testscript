4321958fc2503c9e1324506c05246d216277c78c
tbtri <tbtri@tma.com.vn>
2025-04-25 15:11:40 +0700

    Add modified scripts

--- pages/home_page.py
+++ pages/home_page.py
@@ -4,13 +4,24 @@ class HomePage(BasePage):
     def __init__(self,page):
         super().__init__(page)
         self.Page_Title=page.locator("//h1[text()='Swag Labs']")
-
-
+        self.logout_button=page.locator("//a[@id='logout_sidebar_link']")
+        self.welcome_message=page.locator("//span[@class='title']")
+        self.product_card=page.locator(".inventory_item")
+        self.burger_menu=page.locator("//button[@id='react-burger-menu-btn']")
+        #self.add_to_cart_button=page.locator("//button[@='Add to cart']")
+        self.shopping_cart_badge=page.locator(".shopping_cart_badge")
+    def add_to_cart_by_index(self,index):
+        self.product_card.nth(index).locator(".btn_inventory").click() 
+        
+    def get_product_cards(self):
+        return self.product_card.count()
+    
     def get_login_message(self):
         return self.get_text(self.welcome_message)
     
     def click_logout(self):
-        return self.click(self.logout_button)
+        self.burger_menu.click()
+        self.logout_button.click()
     
     def get_title(self):
         return self.page.title()
--- tests/test_homepage_interactions.py
+++ tests/test_homepage_interactions.py
@@ -5,13 +5,17 @@ from playwright.sync_api import expect
 
 
 def test_all_products_displayed(login_session):
-    page = login_session
-    product_cards = page.locator(".inventory_item")
-    
+    page = HomePage(login_session)
     # Check number of products
-    assert product_cards.count() == 6, "Expected 6 products"
+    assert page.get_product_cards() == 6, "Expected 6 products"
 
 def test_add_to_cart_(login_session):
-    page = login_session
-    page.click("text=Add to cart")
-    assert page.locator(".shopping_cart_badge").inner_text() == "1"
+    page = HomePage(login_session)
+    page.add_to_cart_by_index(0)
+    # Check if cart badge is updated
+    assert page.shopping_cart_badge.inner_text() == "1"
+    
+def test_logout_from_homepage(login_session):
+    page = HomePage(login_session)
+    page.click_logout()
+    expect(login_session).to_have_url("https://www.saucedemo.com/")
