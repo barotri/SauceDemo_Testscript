ce2671373e16a076cc10b8545ad1077eb72724c8
tbtri <tbtri@tma.com.vn>
2025-04-25 14:14:01 +0700

    Add script file

--- pages/home_page.py
+++ pages/home_page.py
@@ -3,7 +3,7 @@ from pages.base_page import BasePage
 class HomePage(BasePage):
     def __init__(self,page):
         super().__init__(page)
-        self.Page_Title=page.locator("//title[text()='Swag Labs']")
+        self.Page_Title=page.locator("//h1[text()='Swag Labs']")
 
 
     def get_login_message(self):
@@ -11,3 +11,6 @@ class HomePage(BasePage):
     
     def click_logout(self):
         return self.click(self.logout_button)
+    
+    def get_title(self):
+        return self.page.title()
