3621d10ded8abf6f304489683446783b36ff6ac0
tbtri <tbtri@tma.com.vn>
2025-04-25 17:47:08 +0700

    Add

--- tests/test_homepage_interactions.py
+++ tests/test_homepage_interactions.py
@@ -1,21 +1,20 @@
 import pytest
-
 from pages.home_page import HomePage
 from playwright.sync_api import expect
 
 
-def test_all_products_displayed(login_session):
+def test_all_products_displayed_TC04(login_session):
     page = HomePage(login_session)
     # Check number of products
     assert page.get_product_cards() == 6, "Expected 6 products"
 
-def test_add_to_cart_(login_session):
+def test_add_to_cart_TC05(login_session):
     page = HomePage(login_session)
     page.add_to_cart_by_index(0)
     # Check if cart badge is updated
     assert page.shopping_cart_badge.inner_text() == "1"
     
-def test_logout_from_homepage(login_session):
+def test_logout_from_homepage_TC06(login_session):
     page = HomePage(login_session)
     page.click_logout()
     expect(login_session).to_have_url("https://www.saucedemo.com/")
